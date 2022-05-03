81832c08c7d8f654ebb4379cca8f14d4b8969456
TabanschiAlex <talex.alexandr@gmail.com>
2022-05-03 14:28:44 +0300

    tests

--- api-gateway/test/app.e2e-spec.ts
+++ api-gateway/test/app.e2e-spec.ts
@@ -3,7 +3,9 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app/AppModule';
 
-describe('AppController (e2e)', () => {
+jest.setTimeout(60000);
+
+describe('MICROSERVICE TESTS (e2e)', () => {
   let app: INestApplication;
   const responses: Record<string, any> = {};
 
@@ -20,64 +22,118 @@ describe('AppController (e2e)', () => {
     await app.close();
   });
 
-  // ------------------------------------- AUTH MICROSERVICE ------------------------------------- //
+  // ---------------------------------------------------------------------------------------------------------------- //
+  // ----------------------------------------------- AUTH MICROSERVICE ---------------------------------------------- //
+  // ---------------------------------------------------------------------------------------------------------------- //
 
-  it('Registration: should return User credentials', () => {
+  it('Registration: should return User credentials', async () => {
     return request(app.getHttpServer())
       .post('/auth/register')
       .send({ email: 'test@domain.com', password: '123456789', passwordConfirmation: '123456789' })
       .expect(201);
   });
 
-  it('Login: should return User credentials', () => {
-    return request(app.getHttpServer())
+  it('Login: should return User credentials', async () => {
+    const response = await request(app.getHttpServer())
       .post('/auth/login')
-      .send({ email: 'root@domain.com', password: '123456789' })
-      .expect(201)
-      .then((value) => (responses.newUser = value.body));
+      .send({ email: 'test@domain.com', password: '123456789' })
+      .expect(201);
+    responses.credentials = response.body;
+
+    return response;
   });
 
-  it('Get Users: should return Users list', () => {
-    return request(app.getHttpServer()).get('/users').set('Authorization', `Bearer ${responses.token}`).expect(200);
-  });
-
-  it('Store User: should return new User', () => {
+  it('Get Users: should return Users list', async () => {
     return request(app.getHttpServer())
       .get('/users')
-      .set('Authorization', `Bearer ${responses.token}`)
-      .send({ email: 'newUser@domain.com', password: '123456789', name: 'TEST' })
-      .expect(201)
-      .then((value) => (responses.newUser = value.body));
-  });
-
-  it('Edit User: should return One User', () => {
-    return request(app.getHttpServer())
-      .get(`/users/${responses.newUser.uuid}`)
-      .set('Authorization', `Bearer ${responses.token}`)
+      .set('Authorization', `Bearer ${responses.credentials.token}`)
       .expect(200);
   });
 
-  it('Update User: should return "User updated successfully"', () => {
+  it('Store User: should return new User', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/users')
+      .set('Authorization', `Bearer ${responses.credentials.token}`)
+      .send({ email: 'newUser@domain.com', password: '123456789', name: 'TEST' })
+      .expect(201);
+    responses.newUser = response.body;
+    console.log(response.body);
+
+    return response;
+  });
+
+  it('Edit User: should return One User', async () => {
+    return request(app.getHttpServer())
+      .get(`/users/${responses.newUser.uuid}`)
+      .set('Authorization', `Bearer ${responses.credentials.token}`)
+      .expect(200);
+  });
+
+  it('Update User: should return "User updated successfully"', async () => {
     return request(app.getHttpServer())
       .put(`/users`)
-      .set('Authorization', `Bearer ${responses.token}`)
+      .set('Authorization', `Bearer ${responses.credentials.token}`)
       .send({ uuid: responses.newUser.uuid, email: responses.newUser.email, name: 'New Name', role: 'admin' })
       .expect(200, 'User updated successfully');
   });
 
-  it('Delete User: should return "User deleted successfully"', () => {
+  it('Delete User: should return "User deleted successfully"', async () => {
     return request(app.getHttpServer())
-      .put(`/users/${responses.newUser.uuid}`)
-      .set('Authorization', `Bearer ${responses.token}`)
+      .delete(`/users/${responses.newUser.uuid}`)
+      .set('Authorization', `Bearer ${responses.credentials.token}`)
       .expect(200, 'User deleted successfully');
   });
+  // ---------------------------------------------------------------------------------------------------------------- //
+  // --------------------------------------------- ARTICLE MICROSERVICE --------------------------------------------- //
+  // ---------------------------------------------------------------------------------------------------------------- //
 
-  // ------------------------------------- ARTICLE MICROSERVICE ------------------------------------- //
+  it('Get Articles: should return Article list', async () => {
+    return request(app.getHttpServer()).get('/articles').expect(200);
+  });
 
-  /*it('Get Articles: should return Article list', () => {
+  it('Store Article: should return new Article', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/articles')
+      .set('Authorization', `Bearer ${responses.credentials.token}`)
+      .send({ title: 'Test article', description: 'supertest', text: 'Article created by supertest' })
+      .expect(201);
+    responses.newArticle = response.body;
+
+    return response;
+  });
+
+  it('Edit Article: should return One Article', async () => {
+    return request(app.getHttpServer()).get(`/articles/${responses.newArticle.id}`).expect(200);
+  });
+
+  it('Update Article: should update article', async () => {
     return request(app.getHttpServer())
-      .put(`/users/${responses.newUser.uuid}`)
-      .set('Authorization', `Bearer ${responses.token}`)
+      .put(`/articles`)
+      .set('Authorization', `Bearer ${responses.credentials.token}`)
+      .send({
+        id: responses.newArticle.id,
+        title: 'Edited title',
+        description: 'Edited description',
+        text: 'Edited text',
+      })
+      .expect(200);
+  });
+
+  it('Delete Article: should delete article', async () => {
+    return request(app.getHttpServer())
+      .delete(`/articles/${responses.newArticle.id}`)
+      .set('Authorization', `Bearer ${responses.credentials.token}`)
+      .expect(200);
+  });
+
+  // ---------------------------------------------------------------------------------------------------------------- //
+  // --------------------------------------------- DELETING TEST ACCOUNT -------------------------------------------- //
+  // ---------------------------------------------------------------------------------------------------------------- //
+
+  it('Delete Test account', async () => {
+    return request(app.getHttpServer())
+      .delete(`/users/${responses.credentials.user.uuid}`)
+      .set('Authorization', `Bearer ${responses.credentials.token}`)
       .expect(200, 'User deleted successfully');
-  });*/
+  });
 });
